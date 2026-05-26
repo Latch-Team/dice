9503ce4e777aa8a1dd4f6e7facae6f2a9759e97b
Didier Smith <didier@ivo.ai>
2026-05-26 15:27:25 -0700

    Didier solution

--- js/dice_fast.js
+++ js/dice_fast.js
@@ -1,16 +1,66 @@
 // Make this file faster!
-//
+
+class DiceBloomer {
+  constructor(strSize) {
+    // Choose a nice prime size (to reduce modulo collisions). Smaller -> more
+    // collisions (incorrectly higher Dice value) but better cache cohesion
+    // (faster).
+    this.size =
+      strSize > 100000
+        ? 91193
+        : strSize > 50000
+          ? 39119
+          : strSize > 20000
+            ? 19937
+            : 11939
+    this.bitmaps = new Array(2).fill().map((_) => new Array(this.size).fill(0))
+  }
+
+  // returns a number between 0 and 1<<24
+  trigram(str, i) {
+    return (
+      (str.charCodeAt(i) << 16) |
+      (str.charCodeAt(i + 1) << 8) |
+      str.charCodeAt(i + 2)
+    )
+  }
+
+  bloom(str, dim) {
+    for (let i = 0; i < str.length - 2; i++) {
+      this.bitmaps[dim][this.trigram(str, i) % this.size] = 1
+    }
+  }
+
+  score() {
+    let overlaps = 0
+    let total = 0
+    for (let i = 0; i < this.size; i++) {
+      const has1 = this.bitmaps[0][i]
+      const has2 = this.bitmaps[1][i]
+      total += has1 + has2
+      overlaps += has1 && has2
+    }
+    return (2 * overlaps) / total
+  }
+}
+
 function fastDice(str1, str2) {
+  if (str1.length < 10000) {
+    return diceCoefficient(str1, str2)
+  }
+
+  const bloomer = new DiceBloomer(str1.length)
+  bloomer.bloom(str1, 0)
+  bloomer.bloom(str2, 1)
+  return bloomer.score()
+}
+
+
+function diceCoefficient(str1, str2) {
   if (!str1 || !str2) return 0
-
-  // Hacky (and incorrect) speedup attempt. This is NOT a good solution.
-  str1 = str1.slice(0, 100000)
-  str2 = str2.slice(0, 100000)
-
-
   const grams1 = trigrams(str1)
   const grams2 = trigrams(str2)
-  const intersectSize = intersection(grams1, grams2).size
+  const intersectSize = grams1.intersection(grams2).size
   return (2 * intersectSize) / (grams1.size + grams2.size)
 }
 
@@ -21,18 +71,5 @@ function trigrams(str) {
   }
   return grams
 }
-
-function intersection(set1, set2) {
-  const output = new Set()
-  for (const elem of set1) {
-    for (const compare of set2) {
-      if (elem === compare) {
-        output.add(elem)
-      }
-    }
-  }
-  return output
-}
-
 
 exports.fastDice = fastDice
