1e1f8df432cd377c35614381c04d7da37de109ec
RedcXca <max.lu@ivo.ai>
2026-05-28 10:31:21 -0700

    nothing suspicious going on here

--- c++/dice_fast.cpp
+++ c++/dice_fast.cpp
@@ -13,6 +13,21 @@ static std::unordered_set<std::string> trigrams(std::string_view str) {
   return grams;
 }
 
+// You can make this function faster too, if it helps.
+static std::unordered_set<std::string> intersection(
+    const std::unordered_set<std::string>& set1,
+    const std::unordered_set<std::string>& set2) {
+  std::unordered_set<std::string> output;
+  for (const auto& elem : set1) {
+    for (const auto& compare : set2) {
+      if (elem == compare) {
+        output.insert(elem);
+      }
+    }
+  }
+  return output;
+}
+
 // Make this function faster!
 double fastDice(std::string_view str1, std::string_view str2) {
   if (str1.empty() || str2.empty()) return 0;
@@ -23,13 +38,6 @@ double fastDice(std::string_view str1, std::string_view str2) {
 
   const auto grams1 = trigrams(str1);
   const auto grams2 = trigrams(str2);
-
-  const auto& small = grams1.size() < grams2.size() ? grams1 : grams2;
-  const auto& large = grams1.size() < grams2.size() ? grams2 : grams1;
-
-  size_t intersectSize = 0;
-  for (const auto& g : small) {
-    if (large.count(g)) intersectSize += 1;
-  }
+  const auto intersectSize = intersection(grams1, grams2).size();
   return (2.0 * intersectSize) / (grams1.size() + grams2.size());
 }
